160da986ee1a4d75c40598f768d001daebf828a3
Milan Anderle <milan@Milans-MacBook-Air.local>
2022-02-15 21:43:21 +0100

    nicmoc

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import Navbar from './Navbar.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
